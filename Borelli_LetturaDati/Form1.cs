4ccb703830353313553c854425e36626f8d6a1a5
Marco Borelli <marco05.borelli@gmail.com>
2022-09-19 09:49:49 +0200

    Aggiunte controllo lunghezze e ricerca record con dimensioni maggiori

--- Borelli_LetturaDati/Form1.cs
+++ Borelli_LetturaDati/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace Borelli_LetturaDati
 {
     public partial class Form1 : Form
@@ -15,6 +17,51 @@ namespace Borelli_LetturaDati
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//conrolla che lunghezze siano uguali
+        {
+            MessageBox.Show(Funzione1e2(0));
+        }
+        private void button2_Click(object sender, EventArgs e)//max riga
+        {
+            MessageBox.Show(Funzione1e2(1));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static string Funzione1e2(int daDoveVieni)
+        {
+            bool helo = true;
+            int a = -1;
+            int lunghezza;
+            using (StreamReader sr = new StreamReader(@"dati.csv", true))
+            {
+                string line = sr.ReadLine();
+                lunghezza = line.Length;
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+
+                    if (a != -1)//se non è prima volta
+                    {
+                        if (a != line.Length)
+                        {
+                            helo = false;
+                            if (daDoveVieni == 1 && a < line.Length)
+                                lunghezza = line.Length;
+                        }
+                    }
+                    a = line.Length;
+                }
+                if (!helo && daDoveVieni == 0)//primi due if se sto controllando che siano lunghi uguali
+                    return "LE LUNGHEZZE SONO DIVERSE";
+                else if (helo && daDoveVieni == 0)
+                    return "LE LUNGHEZZE SONO UGUALI";
+                else//mi interessa solo la lunghezza maggiore
+                    return $"{line.Length}";
+            }
         }
     }
 }
